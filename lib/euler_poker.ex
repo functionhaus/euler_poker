277791d2f941e0391246ae685d3c78bff3f11a90
Mike Zazaian <mike@functionhaus.com>
2019-07-09 20:31:30 -0400

    add ordered hands by type

--- lib/euler_poker.ex
+++ lib/euler_poker.ex
@@ -3,8 +3,21 @@ defmodule EulerPoker do
   Documentation for EulerPoker.
   """
 
+  # Ordered values for ranks, suits, and hands
   @ranks ~w[2 3 4 5 6 7 8 9 10 J Q K A]
   @suits ~w[C D H S]
+  @hands [
+    :high_card,
+    :one_pair,
+    :two_pairs,
+    :three_of_a_kind,
+    :straight,
+    :flush,
+    :full_house,
+    :four_of_a_kind,
+    :straight_flush,
+    :royal_flush
+  ]
 
   @doc """
   Hello world.
